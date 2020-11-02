f28372d4662124dd9b313dfa8f8857141e3ce5b2
Serhii Ovsiuk <sergey.ovsyuk@gmail.com>
2020-11-02 15:05:11 +0200

    Fixed bug related to reading file from jar.

--- src/main/java/com/poc/equifax/demo/web/DemoController.java
+++ src/main/java/com/poc/equifax/demo/web/DemoController.java
@@ -3,11 +3,7 @@ package com.poc.equifax.demo.web;
 import com.poc.equifax.demo.service.MessageReader;
 import com.poc.equifax.demo.web.model.MessagesList;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("membercenter/api/v1/messages")
@@ -15,13 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private final MessageReader messageReader;
+    private static final String COUNT_FILTER_UNREAD = "unread";
 
     @GetMapping()
     public MessagesList getMessage() {
         return messageReader.getMessages();
     }
 
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = "/{id}/mark-read")
     public String markMessageAsRead(@PathVariable("id") Long messageId) {
         if (messageId == null) {
             return "error";
@@ -38,9 +35,12 @@ public class DemoController {
         return String.format("Message with ID: %s exported to PDF", messageId);
     }
 
-    @GetMapping(value = "unread")
-    public String messageCount() {
-        return String.format("%s messages left unread", messageReader.getUnreadMessagesCount());
+    @GetMapping(value = "/count")
+    public String messageCount(@RequestParam(name = "filter") String filter) {
+        if(filter == null || COUNT_FILTER_UNREAD.equals(filter)){
+            return String.format("%s messages left unread", messageReader.getUnreadMessagesCount());
+        }
+        return "Unsupported filter";
     }
 
 }
